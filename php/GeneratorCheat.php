17dd34f9f9cf1886dacb1abe42aacf3559da928e
sxpd7788 <sxpd7788@gmail.com>
2017-02-09 21:03:59 +0800

    init

---
+++ php/GeneratorCheat.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+function dragonStrike($n)
+{
+    if ($n <= 0) {
+        throw new \Exception('意外');
+    }
+
+    while(true) {
+
+        $n--;
+        $yieldExpressionResult = yield $n;
+
+//            var_dump($yieldExpressionResult);
+        if ($yieldExpressionResult) {
+            echo '击打一点生命值，当前对方还剩生命值::', $n ,PHP_EOL, $yieldExpressionResult, PHP_EOL;
+        }
+    }
+}
+
+
+$n = 1000;
+
+$ds = dragonStrike($n);
+
+//var_dump($ds);
+echo '神功准备[rewind()]' , PHP_EOL;
+$ds->rewind();
+
+$startTime = microtime(true);
+
+while($ds->current() >= 0 ) {
+    switch($ds->current()) {
+        case 0:
+            echo '击溃对方！', PHP_EOL;
+            break;
+        case round($n / 2):
+            $ds->send('加油！对方生命值下降一半啦！！！');
+            break;
+        default:
+            echo '击打一点生命值，当前对方还剩生命值:', $ds->current(), PHP_EOL;
+    }
+
+    $ds->next();
+}
+
+$endTime = microtime(true);
+
+echo "击溃对方勇士", bcsub($endTime, $startTime, 4), 's', PHP_EOL;
+
+//echo "times is ", $generatorCheat->times, PHP_EOL;
---
+++ tests/TypeConversionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+final class TypeConversionTest extends PHPUnit\Framework\TestCase {
+
+    public function testStringTypeConversion()
+    {
+        $a = 0 == "a" ? 1 : 2;  //字符串"a"转化为了数字0
+        $this->assertEquals(1, $a);
+    }
+
+    public function testStringTypeConversionA()
+    {
+        $a = 123 == "123foo" ? 1 : 2;
+        $this->assertEquals(1, $a);
+    }
+
+    public function testStringTypeConversionB()
+    {
+        $a = 0123 == "0123foo" ? 1 : 2;  //0123是八进制的83，但是"0123foo"转换成数字变成了123
+        $this->assertEquals(2, $a);
+    }
+
+
+    public function testStringTypeConversionC()
+    {
+        $a = 0 == "a123" ? 1 : 2;  //字符串"a123"转化为了数字0
+        $this->assertEquals(1, $a);
+
+    }
+
+}
