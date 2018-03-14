1862bbb217b088018cdcca7830cf7837c6af56f7
renshuai <sxpd7788@gmail.com>
2018-03-14 18:10:31 +0800

    add

--- php/mathematic/bubbleSort.php
+++ php/mathematic/bubbleSort.php
@@ -4,22 +4,29 @@
 // 它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。
 // 插入排序和冒泡排序在平均和最坏情况下的时间复杂度都是O(n^2)，最好情况下都是O(n)，空间复杂度是O(1)。
 
-$demo_array = array(23,15,43,25,54,2,6,82,11,5,21,32,65);
+//$demo_array = array(23, 15, 43, 25, 54, 2, 6, 82, 11, 5, 21, 32, 65);
+$demo_array = array(23, 15, 43, 25, 54, 2);
 
-// 第一层for循环可以理解为从数组中键为0开始循环到最后一个
-for ($i=0;$i<count($demo_array);$i++) {
-    // 第二层将从键为$i的地方循环到数组最后
-    for ($j=$i+1;$j<count($demo_array);$j++) {
+
+function bubbleSort(array $arr)
+{
+    $count = count($arr);
+    // 第一层for循环可以理解为从数组中键为0开始循环到最后一个
+    for ($i = 0; $i < $count; $i++) {
         // 比较数组中相邻两个值的大小
-        if ($demo_array[$i] > $demo_array[$j]) {
-            $tmp            = $demo_array[$i]; // 这里的tmp是临时变量
-            $demo_array[$i] = $demo_array[$j]; // 第一次更换位置
-            $demo_array[$j] = $tmp;            // 完成位置互换
+        for ($j = $i + 1; $j < $count; $j++) {
+            if ($arr[$i] > $arr[$j]) {
+                $tmp = $arr[$i]; // 这里的tmp是临时变量
+                $arr[$i] = $arr[$j]; // 第一次更换位置
+                $arr[$j] = $tmp;            // 完成位置互换
+            }
         }
     }
+    return $arr;
 }
+
 
 // 打印结果集
 echo '<pre>';
-var_dump($demo_array);
+var_dump(bubbleSort($demo_array));
 echo '</pre>';
